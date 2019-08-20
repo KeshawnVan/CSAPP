1b3b5e7c57e2a0ffd20901d3caf2691ea2f078f0
Fankx <fankx@startimes.com.cn>
2019-08-20 11:45:01 +0800

    test point

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+
+void swap(int *px, int *py) {
+    int temp;
+    temp = *px;
+    *px = *py;
+    *py = temp;
+}
+
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int x = 1;
+    int y;
+    int *px = &x;
+    *px = 2;
+    printf("x : %d \n", x);
+    y = ++*px;
+    printf("x : %d \n", x);
+    printf("y : %d \n", y);
+    int z = 100;
+    printf("before swap x : %d, z: %d \n", x, z);
+    swap(&x, &z);
+    printf("after swap x : %d, z: %d \n", x, z);
 }
---
+++ practice/get_char.c
@@ -0,0 +1,12 @@
+//
+// Created by 10007675 on 2019/4/18 0018.
+//
+#include <stdio.h>
+
+int get() {
+    int c;
+    while ((c = getchar()) != -1) {
+        putchar(c);
+    }
+}
+
